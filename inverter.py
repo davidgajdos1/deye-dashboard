a39a04ea038d52b789565c4041766a75d320fa59
Dávid Gajdoš <50640452+davidgajdos1@users.noreply.github.com>
2026-05-25 10:54:56 +0200

    Update inverter.py

--- inverter.py
+++ inverter.py
@@ -110,10 +110,10 @@ class DeyeInverter:
                 self.connect()
 
             # Solar PV
-            data["pv1_power"] = self.read_register(672)
+            data["pv1_power"] = self.read_register(676) * self.read_register(677)
             time.sleep(0.05)
             if self.config.pv_strings >= 2:
-                data["pv2_power"] = self.read_register(673)
+                data["pv2_power"] = self.read_register(678) * self.read_register(679)
                 time.sleep(0.05)
             else:
                 data["pv2_power"] = 0
